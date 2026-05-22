ce4b78818e54e3cbf933f21b2b346d7b99bf537a
akseloaa-cell <akseloaa@gmail.com>
2026-05-22 15:53:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -154,4 +154,15 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("service-worker.js");
 }
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js").then((reg) => {
+    console.log("SW registered");
+
+    // 🔥 sjekker oppdateringer hvert 30 sek
+    setInterval(() => {
+      reg.update();
+    }, 30000);
+  });
+}
+  
 render();
