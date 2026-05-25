17da3fdf111f4867b0dd5b581b16ad405366d9ce
akseloaa-cell <akseloaa@gmail.com>
2026-05-25 02:29:56 +0200

    Update quest.js

--- quest.js
+++ quest.js
@@ -1,1 +1,25 @@
+export const questTemplates = {
+  daily: [
+    {
+      id: "daily1",
+      name: "Spot 5 biler",
+      goal: 5,
+      reward: 50
+    },
+    {
+      id: "daily2",
+      name: "Spot 10 biler",
+      goal: 10,
+      reward: 100
+    }
+  ],
 
+  weekly: [
+    {
+      id: "weekly1",
+      name: "Spot 50 biler",
+      goal: 50,
+      reward: 500
+    }
+  ]
+};
