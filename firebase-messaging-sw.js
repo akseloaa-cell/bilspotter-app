995f2ad4f0f4bb7ab7a096804e8bfb413d38daa4
akseloaa-cell <akseloaa@gmail.com>
2026-05-26 23:08:37 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,11 +2,11 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "XXX",
-  authDomain: "XXX",
-  projectId: "XXX",
-  messagingSenderId: "XXX",
-  appId: "XXX"
+  apiKey: "AIzaSyDKKkRqNnXsoqE26lr7L9siHDmKJ67bEyI",
+  authDomain: "bilspotter.firebaseapp.com",
+  projectId: "bilspotter",
+  messagingSenderId: "503216789907",
+  appId: "1:503216789907:web:7bfc28a12e94b95ff73066"
 });
 
 const messaging = firebase.messaging();
