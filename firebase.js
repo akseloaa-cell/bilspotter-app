eedb30646c9ea53f09c2ab3b077306f658ae40a0
akseloaa-cell <akseloaa@gmail.com>
2026-05-26 23:07:01 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -22,23 +22,6 @@ const messaging = getMessaging(app);
 /* ================= VAPID ================= */
 const VAPID_KEY = "BMbYF_FmOgLKZDM8kTdvY5BZihwKuMC0at3AYhj7vgJoaVwpe-mKpyCs0pOus0e1dZGK5JwSddr_MxRZm5EjgS4";
 
-/* ================= UTILS ================= */
-function urlBase64ToUint8Array(base64String) {
-  const padding = '='.repeat((4 - base64String.length % 4) % 4);
-  const base64 = (base64String + padding)
-    .replace(/-/g, '+')
-    .replace(/_/g, '/');
-
-  const rawData = atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-
-  return outputArray;
-}
-
 /* ================= ENABLE PUSH ================= */
 export async function enablePush() {
   try {
@@ -49,7 +32,8 @@ export async function enablePush() {
     }
 
     // 🔥 viktig: sørg for service worker
-    const registration = await navigator.serviceWorker.register("/firebase-messaging-sw.js");
+  const registration =
+  await navigator.serviceWorker.register("./firebase-messaging-sw.js");
 
     const token = await getToken(messaging, {
       vapidKey: VAPID_KEY,
