d9e7c92aa702bbe0d7224aed03f654b7ba8a4821
akseloaa-cell <akseloaa@gmail.com>
2026-05-26 21:26:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { questPool } from "./quests.js";
+import { questPool } from "./quests.js"; 
 
 /* ================= DOM ================= */
 const plateInput = document.getElementById("plateInput");
